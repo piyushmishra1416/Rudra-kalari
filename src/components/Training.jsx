91cc901a0b775bdca550710293369a167e315be4
Piyush <piyushmishra1416@gmail.com>
2023-08-18 01:47:03 +0530

    course contents are getting rendered on top of video in the training page

--- src/components/Training.jsx
+++ src/components/Training.jsx
@@ -2,60 +2,50 @@ import React from 'react'
 import kalarivideo2 from "/Users/piyush/course-selling -app/course-selling-app/src/assets/artforms/kalarivideo2.mp4"
 import  { useRef, useEffect } from 'react';
 
-// const App = () => {
-//     const secondDivRef = useRef(null);
-
-//     useEffect(() => {
-//         const observer = new IntersectionObserver(entries => {
-//             if (entries[0].isIntersecting) {
-//                 // Apply animation to the second div when it becomes visible
-//                 secondDivRef.current.classList.add('slide-up-animation');
-//             }
-//         });
-
-//         if (secondDivRef.current) {
-//             observer.observe(secondDivRef.current);
-//         }
-
-//         return () => {
-//             if (secondDivRef.current) {
-//                 observer.unobserve(secondDivRef.current);
-//             }
-//         };
-//     }, []);
-//   }
-
 function Training() {
-  const secondDivRef = useRef(null);
 
-  useEffect(() => {
-    console.log("hiii")
-      const observer = new IntersectionObserver(entries => {
-          if (entries[0].isIntersecting) {
-              // Apply animation to the second div when it becomes visible
-              secondDivRef.current.classList.add('slide-up-animation');
-          }
-      });
+    const secondDivRef = useRef(null);
+    const thirdDivRef = useRef(null);
+    const fourthDivRef = useRef(null);
 
-      if (secondDivRef.current) {
-          observer.observe(secondDivRef.current);
-      }
+    const fadeInObserver = (ref) => {
+        const observer = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) {
+                ref.current.classList.add('fade-in');
+            }
+        });
 
-      return () => {
-          if (secondDivRef.current) {
-              observer.unobserve(secondDivRef.current);
-          }
-      };
-  }, []);
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    };
+
+    useEffect(() => {
+        fadeInObserver(secondDivRef);
+        fadeInObserver(thirdDivRef);
+        fadeInObserver(fourthDivRef);
+    }, []);
+
   return (
    <>
-   <section className='h-screen relative  z-10'>
-      <video  src= {kalarivideo2} autoPlay loop  muted />
+   <section className='h-screen fixed  z--1'>
+      <video  src= {kalarivideo2} autoPlay loop  muted className='w-full h-full object-cover' />
     
    </section>
-   <section className="bg-gray-100  absolute inset-0  ">
-   <div className='h-screen flex items-center space-x-4'>
-     <p className="w-2/3">
+   <section className="bg-gray-100  relative Class
+Properties
+bg-opacity-0	--tw-bg-opacity: 0;
+bg-opacity-5	--tw-bg-opacity: 0.05;
+bg-opacity-10  inset-0 mt-20 mr-16 ">
+   <div className='h-screen flex items-center  space-y-8  inset-0 text-white z-20  space-x-4'>
+    
+     <p className="w-2/3 items-center text-black p-32" >
        All students of Kalari begin by learning the Meythari, which is a
        set of exercises and movements for the Body. While Meythari
        exercises are mainly focused on developing the body’s strength,
@@ -69,11 +59,11 @@ function Training() {
      <img
        src="src/assets/artforms/Meythozhil_(Meypayattu)-20022020134802.jpg"
        alt="meythari"
-       className="w-1/3"
+       className="w-1/3 mr-8 items center"
      />
    </div>
-   <div className='h-screen flex items-center space-x-4 mt-8' ref={secondDivRef}>
-     <p className="w-2/3">
+   <div className='h-screen flex items-center space-x-4  inset-0 text-white z-20 mt-8' ref={secondDivRef}>
+     <p className="w-2/3 items-center p-32">
        Kolthari features the use of various types of sticks – long, short,
        specially fabricated rounded and curved ones – and the student is
        taught to use them to attack as well as to defend. After completing
@@ -84,8 +74,8 @@ function Training() {
      <img src="src/assets/artforms/kolthari.jpg" alt="kolthari" className="w-1/3" />
    </div>
 
-   <div className='h-screen flex items-center space-x-4'>
-     <p className="w-2/3">
+   <div className='h-screen flex items-center inset-0 text-white z-20 space-x-4' ref={thirdDivRef}>
+     <p className="w-2/3 items-center p-32">
        Ankathari is the third phase of training using metal weapons. This
        begins with learning how to use a Dagger (kathi/kadara), followed by
        Sword & Shield. Learning to use the Sword & Shield in an efficient
@@ -96,8 +86,8 @@ function Training() {
      </p>
      <img src="src/assets/artforms/ankathari.jpeg" alt="ankathari"  className="w-1/3"/>
    </div>
-   <div className='h-screen flex items-center space-x-4'>
-     <p className="w-2/3">
+   <div className='h-screen flex items-center inset-0 text-white z-20  space-x-4 ' ref={fourthDivRef}>
+     <p className="w-2/3 items-center p-32">
        Verumkai is extremely important in Kalaripayattu. Literally meaning
        “bare hands,” Verumkai trains a person to use his own body as a
        weapon both to defend himself against an armed or unarmed adversary,
